bce9d77a96d8031cb18aafb86a676c3c2fac5692
stardustsuperwizard <47020213+stardustsuperwizard@users.noreply.github.com>
2020-05-25 09:19:18 -0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, escape, request
+
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/index")
+def index():
+    return "Hello world!"
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=5000, debug=True)
